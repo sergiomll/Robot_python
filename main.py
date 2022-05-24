2b1d0af3c9aff61eb38cc462da21139ec082a873
Sergio <donsergioyo@gmail.com>
2022-05-24 12:28:33 +0200

    Added CNN example

--- main.py
+++ main.py
@@ -1,16 +1,84 @@
-# This is a sample Python script.
+import numpy
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.constraints import maxnorm
+from keras.utils import np_utils
+from keras.datasets import cifar10
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Ejemplo de red convolucional (CNN) para clasificación de imagenes en 10 categorias
+# Avión , Automóvil , Pájaro , Gato , Ciervo , Perro , Rana , Caballo , Barco , Camión
+# Todas las imágenes son de tamaño 32 × 32
 
+seed = 21
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# loading in the data
 
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# normalize the inputs from 0-255 to between 0 and 1 by dividing by 255
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+class_num = y_test.shape[1]
+
+print(X_train.shape[1:])
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=X_train.shape[1:], padding='same'))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Conv2D(128, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Flatten())
+model.add(Dropout(0.2))
+
+model.add(Dense(256, kernel_constraint=maxnorm(3)))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Dense(128, kernel_constraint=maxnorm(3)))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Dense(class_num))
+model.add(Activation('softmax'))
+
+epochs = 25
+optimizer="adam"
+
+model.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=['accuracy'])
+
+print(model.summary())
+
+numpy.random.seed(seed)
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=64)
+
+# Model evaluation
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
